136447fcb94c9d5f5543508be883d6c4c9502689
Ivan <ivan.markov.2013@gmail.com>
2020-11-03 20:29:19 +0300

    2nd verison

--- neuralXOR/neuralXOR.cpp
+++ neuralXOR/neuralXOR.cpp
@@ -1,101 +1,188 @@
 ﻿#include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 #include <ctime>
 #include <cmath>
-#include <A:\\Coding\\Cpp\\Git\\neural_XOR\\neuralXOR\\ohMy.cpp>
+#include <A:\\Coding\\Cpp\\Git\\neural_XOR\\neuralXOR\\ohMy.cpp>    //Поменяйте себе путь к файлу
 
 using namespace std;
 
-int my_xor(int x1, int x2);
-float neuralNetwork(int x1, int x2, int maxEpoch, int target);
-float sigmoidFunction(float x);
-float sigmoidPrime(float x);
+void random_weights(const int n, double w1[], double w2[], double w3[]);
+void get_weights(const int n, double w1[], double w2[], double w3[]);
+void set_weights(const int n, double w1[], double w2[], double w3[]);
+void neural_learning();
+double neuralNetwork(int input[]);
+double sigmoidFunction(double x);
 
 int main()
 {
+    cout << "This is a nerural network that calculates XOR." << endl;
     bool task = true;
     while (task)
-    {
-        cout << "This is a nerural network that calculates XOR." << endl <<
-            "Enter x1, x2: ";
-        int x1, x2;
-        cin >> x1 >> x2;
+    {   
+        ifstream file_w("weights.txt");
+        if (!file_w.is_open())  //если файл не открыт
+        {
+            neural_learning();
+        }
+        else
+        {
+            file_w.close();
+        }
 
-        int target = my_xor(x1, x2);
+        cout << "Enter x1, x2: ";
+        int x[2];
+        cin >> x[0] >> x[1];
 
-        cout << "Enter max Epoch value: ";
-        int maxEpoch;
-        cin >> maxEpoch;
-        cout << neuralNetwork(x1, x2, maxEpoch, target) << endl;
-
+        cout << neuralNetwork(x) << endl;
         task = mojemPovtorit();
     }
     return 0;
 }
 
-int my_xor(int x1, int x2)  //XOR, для того, чтобы знать какой результат должен быть
-{
-    int result;
-    if (x1 == x2)
-    {
-        result = 0;
-    }
-    else
-    {
-        result = 1;
-    }
-    return result;
-}
-
-float neuralNetwork(int x1, int x2, int maxEpoch, int target)
+void random_weights(const int n, double w1[], double w2[], double w3[])
 {
     srand(time(0));     //Настройка генерации чисел от времени
-    const int n = 3;    //Кол-во скрытых слоев
-    float result = 0;   //Выходной результат программы, в случае если кол-во эпох заданное пользователем равно 0
-
-    float w1[n], w2[n], w3[n];
-    for (int i = 0; i < n; i++)     //Цикл, присваивающий рандомные значения весам (диапазон от 0.1 до 0.9)
+    for (int i = 0; i < n; i++)     //Цикл, присваивающий рандомные значения весам (диапазон от -0.5 до 0.5)
     {
-        w1[i] = (1 + rand() % 9) * 0.1;     //Вес от первого входа до скрытого слоя
-        w2[i] = (1 + rand() % 9) * 0.1;     //Вес от второго входа до скрытого слоя
-        w3[i] = (1 + rand() % 9) * 0.1;     //Вес от скрытого слоя до выхода
+        w1[i] = (5 - rand() % 10) * 0.1;     //Вес от первого входа до скрытого слоя
+        w2[i] = (5 - rand() % 10) * 0.1;     //Вес от второго входа до скрытого слоя
+        w3[i] = (5 - rand() % 10) * 0.1;     //Вес от скрытого слоя до выхода
     }
+}
 
+void get_weights(const int n, double w1[], double w2[], double w3[])
+{
+    ifstream file_w("weights.txt");  //
+    for (int i = 0; i < 3 * n; i++)
+    {
+        string num;
+        file_w >> num;
+        if (i < 3)
+        {
+            w1[i] = stof(num);
+        }
+        else if (i < 6)
+        {
+            w2[i - 3] = stof(num);
+        }
+        else
+        {
+            w3[i - 6] = stof(num);
+        }
+    }
+    file_w.close();
+}
+
+void set_weights(const int n, double w1[], double w2[], double w3[])
+{
+    ofstream file_w("weights.txt");
+    for (int i = 0; i < 3 * n; i++)
+    {
+        if (i < 3)
+        {
+            file_w << fixed << w1[i] << endl;
+        }
+        else if (i < 6)
+        {
+            file_w << fixed << w2[i - 3] << endl;
+        }
+        else
+        {
+            file_w << fixed << w3[i - 6] << endl;
+        }
+    }
+    file_w.close();
+}
+
+void neural_learning()
+{
+    const int in = 2;   //Кол-во нейронов на входе
+    const int trainSet = 4; //Кол-во тренировачных сетов
+    const int n = 3;    //Кол-во нейронов в скрытом слое
+    double k = 0.3;  //Коэффициент
+
+    int input[trainSet][in] = { {0, 0}, {0, 1}, {1, 0}, {1, 1} };   //
+    int output[trainSet] = { 0, 1, 1, 0 };  //
+    double w_input[in][n], w_output[n]; //
+    random_weights(n, w_input[0], w_input[1], w_output);    //
+    
+    int maxEpoch = 100000; //Кол-во Эпох
+    int delta_load = maxEpoch / 10, loading = delta_load;   //Переменные для реализации "загрузки"
     for (int epoch = 0; epoch < maxEpoch; epoch++)   //Начало работы самой нейросети 
     {
-        float pre_hiddenLayer[n], hiddenLayer[n], pre_result = 0;   //Расчет значений до скрытого слоя, в скрытом слое и то, что из него выйдет
-        for (int i = 0; i < n; i++)
+        if ((epoch + 1) == loading) //Собственно сама загрузка, 10 звездочек - финал обучения
         {
-            pre_hiddenLayer[i] = x1 * w1[i] + x2 * w2[i];
-            hiddenLayer[i] = sigmoidFunction(pre_hiddenLayer[i]);
-            pre_result += hiddenLayer[i] * w3[i];
+            cout << '*';
+            loading += delta_load;
         }
-        result = sigmoidFunction(pre_result);   //Конечный результат
 
-        float margin_of_error = target - result;    //Сравнение результата с ожидаемым ответом и вычисление погрешности
-        float deltaOutSum = sigmoidPrime(pre_result) * margin_of_error;     //Погрешность суммы до выхода, использование производной функции преобразования
-        float deltaWeights[n], deltaHiddenSum[n];
-        for (int i = 0; i < n; i++)
+        for (int train = 0; train < trainSet; train++)
         {
-            deltaWeights[i] = deltaOutSum / hiddenLayer[i];
-            w3[i] = w3[i] + deltaWeights[i];
+            //Forward
 
-            deltaHiddenSum[i] = deltaOutSum / w3[i] * sigmoidPrime(pre_hiddenLayer[i]);
-            w1[i] = deltaHiddenSum[i] * x1 + w1[i];
-            w2[i] = deltaHiddenSum[i] * x2 + w2[i];
+            double pre_hiddenLayer[n], hiddenLayer[n], pre_result = 0;   //Расчет значений до скрытого слоя, в скрытом слое и то, что из него выйдет
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < in; j++)
+                {
+                    pre_hiddenLayer[i] = input[train][0] * w_input[j][i];
+                }
+                hiddenLayer[i] = sigmoidFunction(pre_hiddenLayer[i]);
+                pre_result += hiddenLayer[i] * w_output[i];
+            }
+            double result = sigmoidFunction(pre_result);   //Конечный результат
+            cout << result << endl;
+
+            //Backward
+
+            double sigma_out = (output[train] - result) * result * (1 - result);     //Погрешность суммы до выхода, использование производной функции преобразования
+            double delta_w_output[n], sigma_in[n];
+            for (int i = 0; i < n; i++)
+            {
+                delta_w_output[i] = k * sigma_out * hiddenLayer[i];
+                sigma_in[i] = delta_w_output[i] * w_output[i] * hiddenLayer[i] * (1 - hiddenLayer[i]);
+
+                w_output[i] += delta_w_output[i];
+            }
+
+            double delta_w[in][n];
+            for (int i = 0; i < in; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    delta_w[i][j] = k * sigma_in[j] * input[train][i];
+                    w_input[i][j] += delta_w[i][j];
+                }
+            }
         }
     }
-    return result;
+    cout << endl;
+    set_weights(n, w_input[0], w_input[1], w_output);
 }
 
-float sigmoidFunction(float x)  //Функция преобразования внутри нейрона
+double neuralNetwork(int input[])
 {
-    float f = 1 / (1 + exp(-x));
-    return f;
+    const int n = 3;    //Кол-во скрытых слоев
+    const int in = 2;
+    double w_input[in][n], w_output[n];
+    get_weights(n, w_input[0], w_input[1], w_output);
+
+    double pre_hiddenLayer[n], hiddenLayer[n], pre_result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            pre_hiddenLayer[i] = input[j] * w_input[j][i];
+        }
+        hiddenLayer[i] = sigmoidFunction(pre_hiddenLayer[i]);
+        pre_result += hiddenLayer[i] * w_output[i];
+    }
+    return sigmoidFunction(pre_result);
 }
 
-float sigmoidPrime(float x)     //Производная этой функции
+double sigmoidFunction(double x)  //Функция преобразования внутри нейрона
 {
-    float f = exp(-x) / pow((1 + exp(-x)), 2);
-    return f;
+    return 1 / (1 + exp(-x));
 }
